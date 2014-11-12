3023a319ea2addbc338c9520f9a96e6f89c7e720
EstebanRguez <esteban_g.rguez@hotmail.com>
2014-11-11 18:00:08 -0600

    Commit inicial

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/esteban/my-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/esteban/my-app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/esteban/my-app/platforms/android/ant-build/classes.dex : \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/home/esteban/my-app/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/home/esteban/my-app/platforms/android/ant-build/dexedLibs/classes-f8b2a321fd4f3a1afc0346b14338e040.jar \
